c1754702096fe4263c58cf79c9e82fbf4b9c3f26
mayankagg9722 <mayankagg9722@gmail.com>
2017-03-19 18:44:52 +0530

    single joy stick added

---
+++ app/src/main/java/com/example/mayankaggarwal/joystick/ShowActivity.java
@@ -0,0 +1,23 @@
+package com.example.mayankaggarwal.joystick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        MainActivity joystick = (MainActivity) findViewById(R.id.myjoystick);
+        joystick.setOnMoveListener(new MainActivity.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                Log.d("tagg",angle+" Str: "+strength);
+            }
+        });
+
+    }
+}
