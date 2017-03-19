3c1e416a0627d78c7ee3ee775c21e4a828b3fc53
mayankagg9722 <mayankagg9722@gmail.com>
2017-03-20 01:02:38 +0530

    learn to add beizer curve

--- app/src/main/java/com/example/mayankaggarwal/joystick/CustomLayout.java
+++ app/src/main/java/com/example/mayankaggarwal/joystick/CustomLayout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -16,6 +17,9 @@ import android.view.View;
 public class CustomLayout extends View {
 
     public Paint myLayout;
+
+    Paint paint;
+    Path path;
 
     public CustomLayout(Context context) {
         super(context);
@@ -39,5 +43,22 @@ public class CustomLayout extends View {
         myLayout.setStyle(Paint.Style.FILL);
 //        Log.d("tagg", String.valueOf(canvas.getWidth()+" "+canvas.getHeight()));
         canvas.drawCircle(canvas.getWidth()-120, canvas.getHeight(), (canvas.getHeight()-70), myLayout);
+
+
+        //**********############BEIZER CURVE#########*************************
+
+//        paint = new Paint();
+//
+//        paint.setStyle(Paint.Style.STROKE);
+//        path = new Path();
+//        paint.setColor(Color.RED);
+//        paint.setStrokeWidth(700);
+//        path.moveTo(0,0);
+////        path.cubicTo(68, 151, 286, 350, 336, 252);
+////        path.quadTo(0,600,canvas.getWidth(),300);
+//
+//        path.quadTo(0,0,canvas.getWidth(),300);
+//        canvas.drawPath(path, paint);
+
     }
 }
